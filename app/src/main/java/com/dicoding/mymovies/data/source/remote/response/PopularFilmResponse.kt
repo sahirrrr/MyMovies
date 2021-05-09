f94d9134d6140997b0cca2c82c8e27533c0e4c12
Sahir <sahirsyatha77@gmail.com>
2021-05-10 01:09:46 +0700

    added popular film response

---
+++ app/src/main/java/com/dicoding/mymovies/data/source/remote/response/PopularFilmResponse.kt
@@ -0,0 +1,11 @@
+package com.dicoding.mymovies.data.source.remote.response
+
+import com.dicoding.mymovies.data.source.local.entity.PopularFilmEntity
+import com.google.gson.annotations.SerializedName
+
+data class PopularFilmResponse(
+
+	@field:SerializedName("results")
+	val results: List<PopularFilmEntity>
+
+)
